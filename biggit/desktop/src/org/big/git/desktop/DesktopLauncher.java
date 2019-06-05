0d7b6b44a3eff5b4e98de81781064cf0bb4d2bc1
sergi_M <smanero.onjob@gmail.com>
2019-06-05 17:35:24 +0200

    Add files via upload

---
+++ biggit/desktop/src/org/big/git/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.big.git.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.big.git.BigGitApp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BigGitApp(), config);
+	}
+}

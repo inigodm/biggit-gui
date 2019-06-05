1b55cad8bd1be8a501e13a939ca835c6eef24ddc
inigo <inigo.delgado@gmail.com>
2019-06-05 23:38:50 +0200

    Mockito working

--- core/test/com/big/git/TestKotlin.kt
+++ core/test/com/big/git/TestKotlin.kt
@@ -5,6 +5,8 @@ import org.junit.Assert.assertTrue
 import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentMatchers.anyString
+import org.mockito.Mockito.*
 
 class TestKotlin{
 
@@ -12,12 +14,26 @@ class TestKotlin{
 
     @Before
     fun init(){
-        bga = BigGitApp()
+        bga = spy(BigGitApp::class.java)
     }
 
     @Test
     fun junit_working(){
         assertTrue("siu" == "siu")
         assertFalse(bga == null)
+    }
+
+    @Test
+    fun mockito_working(){
+        var t = spy(T::class.java)
+        t.foo("lala")
+        verify(t, only()).foo(anyString())
+    }
+
+}
+
+class T{
+    fun foo(fo: String){
+        println(fo)
     }
 }
